ce8549fe0f3de66450d856ec77b6ce041fd3e24a
Srinandan <57226393+srinandan2001@users.noreply.github.com>
2021-07-02 12:57:25 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void ssort(int arr[],int l)
+{
+    int i;
+    for(i=0;i<l;i++)
+    {
+        swap_max(arr,l,i);
+    }
+}
+void swap_max(int arr[],int l,int n)
+{
+    int j;
+    int k;
+    int temp;
+    for(j=k=n; j<l; j++)
+    {
+        if(arr[j] > arr[k])
+        {
+            k = j;
+        }
+    }
+    /*Swapping*/
+    temp = arr[n];
+    arr[n] = arr[k];
+    arr[k] = temp;
+}
